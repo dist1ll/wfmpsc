9f85561f5eb610669fa1c534d53953c27d3e3fa1
Adrian Alic <contact@alic.dev>
2022-12-24 00:00:00 +0100

    Transition to proper integration testing

--- tests/overflow.rs
+++ tests/overflow.rs
@@ -4,16 +4,12 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+/**!
+
+*/
 use wfmpsc;
 
-fn main() {
-    call();
-}
-
-fn call() {
-    let mpscq = wfmpsc::queue!(
-        bitsize: 16,
-        producers: 9,
-        l1_cache: 128
-    );
+#[test]
+pub fn overflow_put() {
+    
 }
