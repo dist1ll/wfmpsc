91e1b91c855c34248a9dbb5279efc2937034b852
Adrian Alic <contact@alic.dev>
2023-01-09 00:00:00 +0100

    Add missing MPL license header to files

--- benches/cfg.rs
+++ benches/cfg.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 /// Creates a compile-time configuration from environment variables. You can
 /// set them either in a shell, or via a `.cargo/config.toml` config file.
 pub const fn cfg_from_env() -> BenchCfg {
